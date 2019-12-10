16d642e034d423fb8a18e33242ec42132f279a22
arthcp <arth@matic.network>
2019-12-10 19:36:31 +0530

    Marketplace page

--- src/components/common/landCard/landCard.js
+++ src/components/common/landCard/landCard.js
@@ -1,74 +1,41 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import {Link } from 'react-router-dom';
 import './land-card.scss'
-// import cardImg from '../assets/images/map3.png'
-// import cardImgBig from '../assets/images/map-big.png'
-import icons from '../../../services/icon-service.js'
-import plazaGreen from '../assets/images/icon-plaza-green.svg'
-import districtBlue from '../assets/images/icon-district-blue.svg'
-import roadGray from '../assets/images/icon-road-gray.svg'
-
-const MapMarker = icons['fa-map-marker']
+import MANA from '../assets/images/MANA.png';
+import WETH from '../assets/images/WETH.png';
+import ETH from '../assets/images/ETH.png';
 
 class LandCard extends React.Component {
   render () {
-    const {
-      name,
-      months,
-      num,
-      x,
-      y,
-      green,
-      blue,
-      gray,
-      mana,
-      manaSymbol,
-      index,
-      my,
-      image_url
-    } = this.props
-    
+    const token = this.props.order_metadata.lowest_sell_order.payment_token.symbol;
     return (
-      <div key={index} className="col-2">
-        <Link key={index} to={my ? '/mycard/'+this.props.id : '/maticcard'} className='land-card h-100'>
+      <div key={this.props.index} className="col-3 my-3">
+        <Link key={this.props.index} to={`/mycard/${this.props.objectID}`} className='land-card h-100'>
           <div className='land-card-preview'>
             <img
-              src={image_url}
+              src={this.props.image_url}
               alt='cardImg'
               className='land-card-preview-image'
             />
           </div>
           <div className='land-card-content'>
-            <div className='land-card-content-text'>
-              <div className='land-card-content-heading'>
-                <div className='land-card-content-title'>{name}</div>
+            <div className="row">
+              <div className="col">
+                <div className="collection-name">{this.props.collections[0].name}</div>
               </div>
-              <div className='land-card-content-date'>{months} MonthsAgo</div>
+              <div className="col ml-auto text-right">
+                <div className="d-inline-flex">
+                  <div className="mr-2">
+                    <img className="token-symbol" src={token === "MANA" ? MANA : token === "WETH" ? WETH : ETH} />
+                  </div>
+                  <div className="">{this.props.order_metadata.lowest_sell_order.payment_token_price}</div>
+                </div>
+              </div>
             </div>
-            <div className='land-card-content-info'>
-              <div className='land-card-content-xy'>
-                {/* <img
-                  src={placeIcon}
-                  alt="placeIcon"
-                  className="land-card-place-icon"
-                /> */}
-                <MapMarker />
-                <p className='land-card-content-x'>{x},</p>
-                <p className='land-card-content-x'>{y}</p>
+            <div className="row">
+              <div className="col">
+                <div className='land-card-content-title'>{this.props.name}</div>
               </div>
-              {green && (
-                <img src={plazaGreen} className='land-card-content-image green' />
-              )}
-              {blue && (
-                <img
-                  src={districtBlue}
-                  className='land-card-content-image blue'
-                />
-              )}
-              {gray && (
-                <img src={roadGray} className='land-card-content-image gray' />
-              )}
             </div>
           </div>
         </Link>
